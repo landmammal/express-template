b9a7fe55ceb3711fc1f98715e3dc4ff6f600b62b
Roger <evolpirate@gmail.com>
2019-04-28 18:32:50 -0400

    installing cors middleware to accept cross origin request for when we build our front end app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 // to access variables use process.env.NAMEOFVARIABLE
-import 'dotenv/config'
+import 'dotenv/config';
+import cors from 'cors';
 import express from 'express';
 
+// starting application
 const app = express();
+
+// application middleware
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello world');
