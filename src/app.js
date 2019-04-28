0bced48877bb53c08443a760a4f1a34a28b27d2a
Roger <evolpirate@gmail.com>
2019-04-28 19:29:01 -0400

    adding our router middleware

--- src/app.js
+++ src/app.js
@@ -2,16 +2,13 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
-
+import routes from '../routes';
 // starting application
 const app = express();
 
 // application middleware
-app.use(cors());
-app.use(express.static('../public'));
-
-app.get('/', (req, res) => {
-    res.send('hello world');
-})
+app.use(cors()); // handles crossorigin request defaul is *
+app.use(express.static('../public')); // where to look for static files 
+app.use(routes); // our routing manager for all requests
 
 module.exports = app;
