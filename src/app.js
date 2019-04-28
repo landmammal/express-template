f4f577c2ed5129cabd7f726809f7b9f0f4ee85a6
Roger <evolpirate@gmail.com>
2019-04-28 18:21:06 -0400

    setting up express application and exporting it to server to handle request

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
 // to access variables use process.env.NAMEOFVARIABLE
 import 'dotenv/config'
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+module.exports = app;
