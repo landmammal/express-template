e7e874f73eb546ba8072d3b04112ae2d3e3b95b8
Roger <evolpirate@gmail.com>
2019-04-30 13:16:45 -0400

    adding registration routes to user

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,15 @@ import express from 'express'
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+// GET /register
+router.get('/register', (req, res, next) => {
     res.send('hello world');
 })
+
+// POST /register
+router.get('/register', (req, res, next) => {
+    res.send('hello world');
+})
+
 
 module.exports = router;
