bf6b2c3f8800ab8e1ac5e62773987c5d4aac02ca
Roger <evolpirate@gmail.com>
2019-04-30 06:33:48 -0400

    adding middleware to parse incoming request

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 // to access variables use process.env.NAMEOFVARIABLE
 import 'dotenv/config';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import express from 'express';
 import routes from '../routes';
 // starting application
 const app = express();
+
+// parse incoming request
+app.use(bodyParser.jason());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // application middleware
 app.use(cors()); // handles crossorigin request defaul is *
