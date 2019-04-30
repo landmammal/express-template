147af57a46ba837d6b7d00da266c7dd8193ed660
Roger <evolpirate@gmail.com>
2019-04-30 19:17:31 -0400

    initializing app with exress before any middelwares

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ import mongoose from 'mongoose';
 import session from 'express-session';
 import routes from '../routes';
 
+// starting application
+const app = express();
+
 // use session for tracking logins
 app.use(session({
     secret: process.env.SECRETSESSION,
@@ -17,11 +20,8 @@ app.use(session({
 // mongod connection
 mongoose.connect('mongodb://localhost:27017/my_database', { useNewUrlParser: true, useCreateIndex: true });
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
 
-// starting application
-const app = express();
 
 // parse incoming request
 app.use(bodyParser.json());
