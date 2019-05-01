1f9cc4a40adb229f9bacf3fd87a0013d52b1106c
Roger <evolpirate@gmail.com>
2019-04-30 23:37:52 -0400

    adding custome middleware for checking if a user is looged in and if a user is authorize

---
+++ middleware/index.js
@@ -0,0 +1,19 @@
+function loggedOut (req, res, next) {
+    if (req.session && req.session.userId){
+        return res.redirect('./profile');
+    }
+    return next();
+};
+
+function requiredLogin (req, res, next) {
+    if (req.session && req.session.userId) {
+        return next();
+    } else {
+        let err = new Error("You must be looged in to view this page.")
+        err.status = 401;
+        return next(err);
+    }
+}
+
+module.exports.loggedOut = loggedOut;
+module.exports.requiredLogin = requiredLogin;
