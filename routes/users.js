15abd51c963655e50b301477fa66726ddb28abb4
Roger <evolpirate@gmail.com>
2019-05-01 17:45:38 -0400

    refactoring routes with controllers methods

--- routes/users.js
+++ routes/users.js
@@ -1,101 +1,29 @@
 import express from 'express';
-import User from '../models/user';
+import usersController from '../controllers/users';
 import mid from '../middleware/index';
 
 const router = express.Router();
 
-// example secure profile route
-router.get('/profile', mid.requiredLogin, (req, res, next) => {
-    User.findById(req.session.userId)
-        .exec(function(err, user){
-            if (err) {
-                return next(err);
-            } else {
-                return res.send('Hello ' + user.name + '.')
-            }
-        })
-});
+// Get /profile with authorization 
+router.get('/profile', mid.requiredLogin, usersController.userProfile);
 
-// GET /login
+// GET /login render view
 router.get('/login', mid.loggedOut, (req, res, next) => {
     res.send('hello login');
 });
 
-// POST /login
-router.post('/login', (req, res, next) => {
-    if(req.body.email && req.body.password){
-        User.authenticate(req.body.email, req.body.password, function(err, user){
-            if (err || !user) {
-                let err = new Error('Wrong email or password');
-                err.status = 401;
-                return next(err);
-            } else {
-                req.session.userId = user._id;
-                return res.redirect('./profile');
-            }
-        });
-    } else {
-        let err = new Error('Email and Password are required');
-        err.status = 401;
-        return next(err);
-    }
-});
+// POST /login handles users log in
+router.post('/login', usersController.loginUser);
 
 // GET /logout 
-router.get('/logout', (req, res, next) => {
-    if (req.session) {
-        // delete session
-        req.session.destroy( (err) => {
-            if (err){
-                return next(err)
-            } else {
-                res.redirect('/')
-            }
-        })
-    }
-});
+router.get('/logout', usersController.userLogout);
 
 // GET /register
 router.get('/register', mid.loggedOut, (req, res, next) => {
     res.send('hello register');
 });
 
-// POST /register
-router.post('/register', (req, res, next) => {
-    if(req.body.email &&
-       req.body.name &&
-       req.body.password && 
-       req.body.confirmPassword){
-
-        // confirm passwords match 
-        if(req.body.password !== req.body.confirmPassword){
-            let err = new Error("Passwords dont match.");
-            err.status = 400;
-            return next(err);
-        }
-
-        // create object with form input
-        let userDetails = {
-            email: req.body.email,
-            name: req.body.name,
-            password: req.body.password
-        }
-
-        // insert new user into mongo db
-        User.create(userDetails, (err, user) => {
-            if (err){
-                return next(err);
-            } else{
-                req.session.userId = user._id;
-                return res.redirect('/');
-            }
-        });
-       } else{
-           let err = new Error("All fields required.");
-           err.status = 400;
-           return next(err);
-       }
-})
-
+// POST /register registers users to app
+router.post('/register', usersController.registerUser)
 
 module.exports = router;
